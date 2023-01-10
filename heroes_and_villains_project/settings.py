919b4da1d9b9fe836d88b5964a12398386fc59fc
unknown <schuldjack@gmail.com>
2023-01-10 16:10:24 -0600

    Added framework and super_types to apps + Migrated super_types Model

--- heroes_and_villains_project/settings.py
+++ heroes_and_villains_project/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'super_types',
 ]
 
 MIDDLEWARE = [
@@ -107,3 +109,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from heroes_and_villains_project.local_settings import *
+except ImportError:
+    pass
